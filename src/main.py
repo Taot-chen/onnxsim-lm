08c77633408a3bccdbb7c6b0f8f13a48dfa2000c
taot <1624024615@qq.com>
2024-11-20 14:21:23 +0800

    [fix](src): bugfix for main parameters
        1 bugfix for main parameters

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import argparse
 
 def run() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("onnx_input", help="ONNX model to simplify")
-    parser.add_argument("out_dir", default="./onnx/", help="output dir to save simplified onnx model")
+    parser.add_argument("--onnx_input", help="ONNX model to simplify")
+    parser.add_argument("--out_dir", default="./onnx/", help="output dir to save simplified onnx model")
     args = parser.parse_args()
 
     assert (args.onnx_input[-5:] == ".onnx"), f"param onnx_input must end with .onnx"
